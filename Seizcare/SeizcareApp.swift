e255fcba99c949260bede45aecfad649c4c511af
GS Agrawal <gsagrawal@GSs-MacBook-Pro.local>
2026-04-14 16:32:45 +0530

    Initializing seizcare swiftUI project

---
+++ Seizcare/SeizcareApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeizcareApp.swift
+//  Seizcare
+//
+//  Created by GS Agrawal on 30/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SeizcareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
